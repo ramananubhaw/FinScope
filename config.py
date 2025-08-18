6411d17ec1286a2aec9f2b95b90a34918669dc46
yuvrajpradhan <yuvraj.pradhan.2004@gmail.com>
2025-08-18 15:45:44 +0530

    Initial commit

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_KEY_GEMINI = "YAIzaSyCHjb_JsSOcJ0MgkJEcSwAyOb7OSBG-yVo"
-API_KEY_NEWS = "TY1OOKQIDSR54LZR"
+API_KEY_GEMINI = "45he9fubprwiuvbpiefbfiboubyVo"
+API_KEY_NEWS = "TY1OOKQID545yt4LZR"
 TICKER = "AAPL" # The stock ticker you want to analyze (e.g., "AAPL", "GOOGL", "MSFT")
 START_DATE = "2020-01-01" # Start date for historical data
 END_DATE = "2024-01-01" # End date for historical data
